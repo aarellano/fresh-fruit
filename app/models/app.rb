b44eca1f8a1b10e6c56b4ef6e2a8e99c90e451b3
Andres Arellano <andres.arellano@gmail.com>
2013-08-06 13:32:20 -0400

    Improves App.load_status logic

--- app/models/app.rb
+++ app/models/app.rb
@@ -3,14 +3,18 @@ class App < ActiveRecord::Base
 	has_many :app_statuses
 
 	def self.read_status
-		`top -p $(pgrep -d',' -f 'unicorn master') -n 1 -bc > log/apps.log`
+		`top -p $(pgrep -d',' -f '^unicorn master') -n 1 -bc > "#{::Rails.root.join('tmp', 'apps.log')}"`
 	end
 
 	def self.load_status
-		IO.readlines('log/apps.log').drop(7).each do |line|
+		IO.readlines("#{::Rails.root.join('tmp', 'apps.log')}").drop(7).each do |line|
 			next if line.strip.empty?
 			line_split = line.split
-			App.where(name: line_split[14].split("/")[4]) << AppStatus.new(cpu: line_split[8].to_f, memory: line_split[9].to_f)
+			app_name = line_split[14].split("/")[4]
+			if (!app_name.nil? && (app = App.find_by(name: line_split[14].split("/")[4])).nil?
+				app = App.create(name: app_name)
+			end
+			app << AppStatus.new(cpu: line_split[8].to_f, memory: line_split[9].to_f)
 		end
 	end
 end
