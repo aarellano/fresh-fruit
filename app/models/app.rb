d48f9f528ad1fa5dc9e1abab2d009764056552a4
Andres Arellano <andres.arellano@gmail.com>
2013-08-06 12:31:42 -0400

    Replaces whenever by clockwork
    This is a solution that can work with the rails environment without
    starting a new one each time, as it is with rake tasks or rails runners.

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,16 @@
 class App < ActiveRecord::Base
 	belongs_to :host
 	has_many :app_statuses
+
+	def self.read_status
+		`top -p $(pgrep -d',' -f 'unicorn master') -n 1 -bc > log/apps.log`
+	end
+
+	def self.load_status
+		IO.readlines('log/apps.log').drop(7).each do |line|
+			next if line.strip.empty?
+			line_split = line.split
+			App.where(name: line_split[14].split("/")[4]) << AppStatus.new(cpu: line_split[8].to_f, memory: line_split[9].to_f)
+		end
+	end
 end
--- config/schedule.rb
+++
@@ -1,5 +0,0 @@
-set :output, "log/cron.log"
-every 1.minute do
-	rake "read_status"
-	rake "load_status"
-end
