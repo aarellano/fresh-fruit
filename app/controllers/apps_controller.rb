a3e01d598877ea72a155e080175b374bc6756874
Andres Arellano <andres.arellano@gmail.com>
2013-08-06 13:52:54 -0400

    Adds show action for App

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,5 +1,17 @@
 class AppsController < ApplicationController
+	before_action :set_app, only: [:show]
+
 	def index
 		@apps = App.all
 	end
+
+	def show
+	end
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_app
+			@app = App.find(params[:id])
+    end
+
 end
